b7bee95cc68872ac7ce1313abb56d6a73c08ff18
Paweł Dąbrowski <dziamber@gmail.com>
2018-05-06 04:32:08 +0200

    #2 Update Chartable::Queries::Monthly and add support for SQLite

--- lib/chartable/queries/monthly.rb
+++ lib/chartable/queries/monthly.rb
@@ -3,10 +3,18 @@ module Chartable
     class Monthly
       # It returns analytics data for the monthly period.
       # Example output: `{"November 2018" => 1, "October 2018" => 1}`
+      # SQLite does not support DATE_FORMAT function so a little hack is needed
       #
       # @return [Hash]
       def self.call(scope, on:)
-        scope.group("DATE_FORMAT(#{on},'%M %Y')").size
+        if ActiveRecord::Base.connection.class.to_s.match(/sqlite/i)
+          scope.group("strftime('%m %Y', #{on})").size.transform_keys do |key|
+            month_number = key.match(/^[0-9]{2}(?= )/).to_s
+            key.gsub(month_number, Date::MONTHNAMES[month_number.to_i])
+          end
+        else
+          scope.group("DATE_FORMAT(#{on},'%M %Y')").size
+        end
       end
     end
   end
