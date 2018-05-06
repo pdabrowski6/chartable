1b976471cf203970facaac60bc1e57cfea321a06
Paweł Dąbrowski <dziamber@gmail.com>
2018-05-06 05:18:56 +0200

    Update Chartable::Queries::Yearly and add support for PostgreSQL

--- lib/chartable/queries/yearly.rb
+++ lib/chartable/queries/yearly.rb
@@ -8,6 +8,8 @@ module Chartable
       def self.call(scope, on:)
         if ActiveRecord::Base.connection.class.to_s.match(/sqlite/i)
           scope.group("cast(strftime('%Y', #{on}) as decimal)").size
+        elsif ActiveRecord::Base.connection.class.to_s.match(/postgresql/i)
+          scope.group("cast(to_char(#{on},'YYYY') as integer)").size
         else
           scope.group("YEAR(#{on})").size
         end
