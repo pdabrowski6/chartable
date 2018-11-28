ca726ce8c65f7bf916ccc9cf723eeb78ba94825a
Paweł Dąbrowski <dziamber@gmail.com>
2018-11-28 14:52:15 +0100

    #8 Update weekly query and respect given order

--- lib/chartable/queries/weekly.rb
+++ lib/chartable/queries/weekly.rb
@@ -5,13 +5,17 @@ module Chartable
       # Example output: `{"01/28/2018 - 02/03/2018" => 1, "02/11/2018 - 02/17/2018" => 1}`
       #
       # @return [Hash]
-      def self.call(scope, on:)
+      def self.call(scope, on:, order:)
         if ActiveRecord::Base.connection.class.to_s.match(/sqlite/i)
           scope.group("strftime('%m/%d/%Y', date(#{on}, 'weekday 0', '-7 days')) || ' - ' || strftime('%m/%d/%Y', date(#{on}, 'weekday 0', '-1 days'))").size
         elsif ActiveRecord::Base.connection.class.to_s.match(/postgresql/i)
-          scope.group("concat(to_char(date_trunc('week', #{on}) + '-1 day', 'MM/DD/YYYY'), ' - ', to_char(date_trunc('week', #{on}) + '5 days', 'MM/DD/YYYY'))").size
+          scope
+            .group("concat(to_char(date_trunc('week', #{on}) + '-1 day', 'MM/DD/YYYY'), ' - ', to_char(date_trunc('week', #{on}) + '5 days', 'MM/DD/YYYY'))")
+            .order("concat(to_char(date_trunc('week', #{on}) + '-1 day', 'MM/DD/YYYY'), ' - ', to_char(date_trunc('week', #{on}) + '5 days', 'MM/DD/YYYY')) #{order}").size
         else
-          scope.group("CONCAT(DATE_FORMAT(DATE(DATE_ADD(#{on}, INTERVAL(1-DAYOFWEEK(#{on})) DAY)), '%m/%d/%Y'), ' - ', DATE_FORMAT(DATE(DATE_ADD(#{on}, INTERVAL(7-DAYOFWEEK(#{on})) DAY)),'%m/%d/%Y'))").size
+          scope
+            .group("CONCAT(DATE_FORMAT(DATE(DATE_ADD(#{on}, INTERVAL(1-DAYOFWEEK(#{on})) DAY)), '%m/%d/%Y'), ' - ', DATE_FORMAT(DATE(DATE_ADD(#{on}, INTERVAL(7-DAYOFWEEK(#{on})) DAY)),'%m/%d/%Y'))")
+            .order("CONCAT(DATE_FORMAT(DATE(DATE_ADD(#{on}, INTERVAL(1-DAYOFWEEK(#{on})) DAY)), '%m/%d/%Y'), ' - ', DATE_FORMAT(DATE(DATE_ADD(#{on}, INTERVAL(7-DAYOFWEEK(#{on})) DAY)),'%m/%d/%Y')) #{order}").size
         end
       end
     end
--- spec/lib/chartable/queries/weekly_spec.rb
+++ spec/lib/chartable/queries/weekly_spec.rb
@@ -6,7 +6,7 @@ describe Chartable::Queries::Weekly do
       Timecop.travel(Date.parse('02/02/2018')) { FactoryBot.create :user }
       Timecop.travel(Date.parse('15/02/2018')) { FactoryBot.create :user }
 
-      expect(described_class.call(User.all, on: 'created_at')).to eq({"01/28/2018 - 02/03/2018" => 1, "02/11/2018 - 02/17/2018" => 1})
+      expect(described_class.call(User.all, on: 'created_at', order: 'desc')).to eq({"01/28/2018 - 02/03/2018" => 1, "02/11/2018 - 02/17/2018" => 1})
     end
   end
 end
