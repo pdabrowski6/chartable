cdd12f40b173fee556dc846534aa43297248eeb9
Paweł Dąbrowski <dziamber@gmail.com>
2018-11-28 14:52:26 +0100

    #8 Update monthly query and respect given order

--- lib/chartable/queries/monthly.rb
+++ lib/chartable/queries/monthly.rb
@@ -6,16 +6,16 @@ module Chartable
       # SQLite does not support DATE_FORMAT function so a little hack is needed
       #
       # @return [Hash]
-      def self.call(scope, on:)
+      def self.call(scope, on:, order:)
         if ActiveRecord::Base.connection.class.to_s.match(/sqlite/i)
           scope.group("strftime('%m %Y', #{on})").size.transform_keys do |key|
             month_number = key.match(/^[0-9]{2}(?= )/).to_s
             key.gsub(month_number, Date::MONTHNAMES[month_number.to_i])
           end
         elsif ActiveRecord::Base.connection.class.to_s.match(/postgresql/i)
-          scope.group("to_char(#{on},'FMMonth YYYY')").size
+          scope.group("#{on}, to_char(#{on},'FMMonth YYYY')").order("#{on} #{order}").size
         else
-          scope.group("DATE_FORMAT(#{on},'%M %Y')").size
+          scope.group("#{on}, DATE_FORMAT(#{on},'%M %Y')").order("#{on} #{order}").size
         end
       end
     end
